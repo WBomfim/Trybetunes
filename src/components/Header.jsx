7a232004ff784c56cda2b45713cccf6263b1dbd0
WBomfim <willianbomfim@hotmail.com>
2022-04-19 19:28:21 -0300

    feat(Links): Adicionados links de navegação ao header da pagina -Junto com os links também foi adicionado css para base da estilização

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 import './Header.css';
@@ -20,17 +21,34 @@ class Header extends Component {
     this.setState({ loading: true });
     const userObject = await getUser();
     const { name } = userObject;
-    this.setState({
-      userName: name,
-      loading: false,
-    });
+    this.setState({ userName: name, loading: false });
   }
 
   render() {
     const { userName, loading } = this.state;
     return (
       <header data-testid="header-component">
-        { loading ? <Loading /> : <p data-testid="header-user-name">{ userName }</p> }
+        { loading
+          ? <Loading />
+          : (
+            <div>
+              <div className="title">
+                <h1>Trybetunes</h1>
+                <p data-testid="header-user-name">{ userName }</p>
+              </div>
+              <nav>
+                <div>
+                  <Link data-testid="link-to-search" to="/search">Pesquisa</Link>
+                </div>
+                <div>
+                  <Link data-testid="link-to-favorites" to="/favorites">Favoritas</Link>
+                </div>
+                <div>
+                  <Link data-testid="link-to-profile" to="/profile">Perfil</Link>
+                </div>
+              </nav>
+            </div>
+          )}
       </header>
     );
   }
