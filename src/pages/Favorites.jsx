43095646170e15f5e26babb20712f2a74b742332
WBomfim <willianbomfim@hotmail.com>
2022-04-18 22:19:35 -0300

    feat: Inserido os componentes e rotas - Criado o diretório pages com os componentes solicitados e inserido suas respsctivas rotas no componente App.

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Favorites extends Component {
+  render() {
+    return (
+      <div className="Favorites" data-testid="page-favorites">
+        Favorites
+      </div>
+    );
+  }
+}
+
+export default Favorites;
