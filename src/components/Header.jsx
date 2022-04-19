16ed0f49f02c6f85ca3ace01a8ed500e33b866e1
WBomfim <willianbomfim@hotmail.com>
2022-04-19 16:44:00 -0300

    feat(Header): Desenvolvido o componente Header - Criada as funções para buscar e exibir o nome de usuario no header, também criado um arquivo css para o componente.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
+import './Header.css';
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: '',
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.getNameUser();
+  }
+
+  getNameUser = async () => {
+    this.setState({ loading: true });
+    const userObject = await getUser();
+    const { name } = userObject;
+    this.setState({
+      userName: name,
+      loading: false,
+    });
+  }
+
   render() {
+    const { userName, loading } = this.state;
     return (
       <header data-testid="header-component">
-        <h1>Header</h1>
+        { loading ? <Loading /> : <p data-testid="header-user-name">{ userName }</p> }
       </header>
     );
   }
