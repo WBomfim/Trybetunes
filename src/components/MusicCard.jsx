09f852414f3d4fca1a6ac9c5d094c10432a5541a
WBomfim <willianbomfim@hotmail.com>
2022-04-20 21:50:33 -0300

    feat: Criado dentro do componente MusicCard a logica para favoritar as músicas

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,19 +1,62 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 import './MusicCard.css';
 
 class MusicCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      favorite: false,
+      loading: false,
+    };
+  }
+
+  hendleFavorite = ({ target }) => {
+    this.setState({
+      favorite: target.checked,
+    }, () => this.setFavorite());
+  }
+
+  setFavorite = async () => {
+    this.setState({ loading: true });
+    const { favorite } = this.state;
+    const { music } = this.props;
+    if (favorite) {
+      await addSong(music);
+    }
+    this.setState({ loading: false });
+  }
+
   render() {
     const { music } = this.props;
-    const { trackName, previewUrl } = music;
+    const { trackName, previewUrl, trackId } = music;
+    const { favorite, loading } = this.state;
+
     return (
       <div className="MusicCard">
-        <p>{ trackName }</p>
-        <audio data-testid="audio-component" src={ previewUrl } controls>
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
-          <code>audio</code>
-        </audio>
+        { loading && <Loading /> }
+        { !loading && (
+          <>
+            <p>{ trackName }</p>
+            <audio data-testid="audio-component" src={ previewUrl } controls>
+              <track kind="captions" />
+              O seu navegador não suporta o elemento
+              <code>audio</code>
+            </audio>
+            <label htmlFor="favorite">
+              <input
+                id="favorite"
+                type="checkbox"
+                checked={ favorite }
+                onChange={ (event) => this.hendleFavorite(event) }
+                data-testid={ `checkbox-music-${trackId}` }
+              />
+              Favorita
+            </label>
+          </>
+        )}
       </div>
     );
   }
@@ -23,6 +66,7 @@ MusicCard.propTypes = {
   music: PropTypes.shape({
     trackName: PropTypes.string,
     previewUrl: PropTypes.string,
+    trackId: PropTypes.number,
   }).isRequired,
 };
 
